5503cdef77b056bc7d6f83d0580f315099adac7b
Ryanbritt <britt.ryant@gmail.com>
2018-02-22 06:35:52 -0500

    created main server.js file, required all middleware, and set up a fallback (404: not found) route

---
+++ server.js
@@ -0,0 +1,45 @@
+//This is the main server.js for my Unit_02 project
+
+//Require packages for the main server.js
+
+const express = require(`express`);
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const path = require('path');
+const mainRouter = (`./routes/main-routes.js`)
+//confiure the port 
+const PORT = process.env.PORT || 3000;
+
+
+//Start the app by initalizing express
+
+const app = express();
+
+//Use method override 
+
+app.use(methodOverride(`_method`));
+
+//configure the app
+app.use(logger(`dev`));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+//static configuration
+
+app.use(express.static(`public`));
+
+//view the configuration
+app.set(`views`, path.join(__dirname, `views`));
+app.set(`view engine`, `ejs`);
+
+
+app.get(`*`, (req, res) => {
+	res.status(404).send(`404 : NOT FOUND  💔`)
+})
+
+
+//set a lisener event for the port
+app.listen(PORT, () => {
+	console.log(`Up and running, listening on port 🙊 ${PORT} in environemnt 🌏 ${process.env.NODE_ENV || 'Development'}`)
+})
