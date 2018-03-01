7f09c66c0f039a9556de84fc2d273b5c5d2b9068
Ryanbritt <britt.ryant@gmail.com>
2018-03-01 16:25:08 -0500

    close but need to walk away for a little bit

--- controllers/main_controller/weatherController.js
+++ controllers/main_controller/weatherController.js
@@ -5,17 +5,23 @@ const request = require('request');
 module.exports = {
 
  	weatherRequest(req, res, next){
- 		rp(`https://api.darksky.net/forecast/7649f2e71b4e98486f4f945d6053ebe0/39.419390, -72.244713?exclude=minutely,daily,hourly,flags`)
- 			.then(body => {
- 			res.locals.weather = JSON.parse(body)
-     		console.log(res.locals.weather);
-     		console.log(res.locals.weather.latitude)
- 			})
- 			.catch(err => {
-     			console.log(err);
- 			});
+ 		console.log(res.locals.location)
+ 		let newArr = [];
+ 		res.locals.location.forEach(loc => {
+ 			// console.log(`https://api.darksky.net/forecast/7649f2e71b4e98486f4f945d6053ebe0/${loc.lat_deg}.${loc.lat_dec},-${loc.lon_deg}.${loc.lon_dec}?exclude=minutely,daily,hourly,flags`)
+	 		rp(`https://api.darksky.net/forecast/7649f2e71b4e98486f4f945d6053ebe0/${loc.lat_deg}.${loc.lat_dec},-${loc.lon_deg}.${loc.lon_dec}?exclude=minutely,daily,hourly,flags`)
+	 			.then(body => {
+	 			res.locals.weather = JSON.parse(body)
+	     		console.log(res.locals.weather);
+	     		console.log(res.locals.weather.latitude)
+	 			})
+	 			.catch(err => {
+	     			console.log(err);
+	 			});
+ 		})
  	}
 }
+
 
 
 
--- routes/weather-router.js
+++ routes/weather-router.js
@@ -1,8 +1,10 @@
 const weatherRouter = require('express').Router();
 const weatherController = require(`../controllers/main_controller/weatherController.js`)
+const mainController = require(`../controllers/main_controller/mainController.js`);
 
 
-weatherRouter.get(`/`, weatherController.weatherRequest)
+weatherRouter.route(`/`)
+	.get(mainController.getListOfLocations, weatherController.weatherRequest)
 
 
 
